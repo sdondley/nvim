a0ec1d55b510dedeb9a186dc07f6820a98fb7d6d
Steve Dondley <s@dondley.com>
2022-02-23 11:29:08 -0500

    add autopairs config

---
+++ nvim/lua/my_autopairs_cfg.lua
@@ -0,0 +1,15 @@
+require("nvim-autopairs").setup({
+	fast_wrap = {
+		map = "<leader>e",
+		--chars = { '{', '[', '(', '"', "'" },
+		--pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+		--offset = 0, -- Offset from pattern match
+		--end_key = '$',
+		--keys = 'qwertyuiopzxcvbnmasdfghjkl',
+		--check_comma = true,
+		--highlight = 'Search',
+		--highlight_grey='Comment'
+	},
+})
+
+
