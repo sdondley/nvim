8d5d0b0524dd5cabaa61038ca46d80b68c06e411
Steve Dondley <s@dondley.com>
2022-02-26 15:32:19 -0500

    add plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -11,7 +11,8 @@ return require('packer').startup( {
 	function()
 		use 'wbthomason/packer.nvim'
 		use { 'christoomey/vim-tmux-navigator', opt = false }
-	end,
+		use { 'neoclide/coc.nvim', opt = false, branch = 'release', ft = 'php' }
+		use { 'rayburgemeestre/phpfolding.vim', opt = false, branch = 'release', ft = 'php' } end,
 	config = {
 		opt_default = true,
 		compile_path = util.join_paths(vim.fn.stdpath('config'), 'compiled_packer_config', 'packer_compiled.lua'),
