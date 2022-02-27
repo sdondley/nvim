fbafc7695bd549baf755bed9dc2e121c63cd58ed
Steve Dondley <s@dondley.com>
2022-02-27 14:44:20 -0500

    get lsp working

--- nvim/lua/lsp_config_packer.lua
+++ nvim/lua/lsp_config_packer.lua
@@ -1,4 +1,3 @@
-vim.cmd[[packadd nvim-lspconfig]]
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -19,7 +18,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-\\>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
@@ -35,6 +34,11 @@ end
 local servers = { 'perlpls' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
+    settings = { 
+    perl = { 
+        perlcritic = { enabled = true }
+    } 
+  },
 	on_attach = on_attach,
 	flags = {
 		-- This will be the default in neovim 0.7+
