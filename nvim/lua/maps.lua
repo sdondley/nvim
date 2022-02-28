dc00bd1cd207f8f231f85726e715c6494be576c5
Steve Dondley <s@dondley.com>
2022-02-27 19:36:52 -0500

    remove blank line

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -4,7 +4,6 @@ nnoremap <F3> :w<CR>:execute "silent !~/bin/refresh_safari quick"<CR>
 nnoremap ;tn :tabnew<space>
 nnoremap <leader>;so :so ~/.config/nvim/init.lua<cr>
 nnoremap <silent>  <f5>  :call ToggleSyntax()<CR>
-
 " turn off highlighting
 nnoremap \\ :noh<cr>
 inoremap <Leader><Leader> <c-x><c-f>
