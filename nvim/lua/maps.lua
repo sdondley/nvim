9dc47c66dbc9bfdec7b7230fae0a1e6e83299adc
Steve Dondley <s@dondley.com>
2022-02-27 19:37:50 -0500

    fix formatting and remove map

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -4,8 +4,9 @@ nnoremap <F3> :w<CR>:execute "silent !~/bin/refresh_safari quick"<CR>
 nnoremap ;tn :tabnew<space>
 nnoremap <leader>;so :so ~/.config/nvim/init.lua<cr>
 nnoremap <silent>  <f5>  :call ToggleSyntax()<CR>
+
 " turn off highlighting
 nnoremap \\ :noh<cr>
-inoremap <Leader><Leader> <c-x><c-f>
+
 nnoremap <leader>q :q<cr>
 ]]
